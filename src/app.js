b5cc4bee13890c23636a9d66dcbaa4e5f844c6c1
amaanmohd047 <amaanmohd047@gmail.com>
2024-03-29 05:57:44 +0530

    added dummy data

--- src/app.js
+++ src/app.js
@@ -1,13 +1,41 @@
-import express from "express";
+const fs = require("fs");
+const express = require("express");
 
 const app = express();
 
-app.get("/", (req, res) => {
+app.use(express.json());
+
+const tours = JSON.parse(
+  fs.readFileSync(`${__dirname}/data/tours-simple.json`)
+);
+
+app.get("/api/v1/tours", (req, res) => {
   res.status(200).json({
-    message: "Hello, World! ",
-    number: 25243,
-    port: port,
+    status: "success",
+    results: tours.length,
+    data: {
+      tours: tours,
+    },
   });
+});
+
+app.post("/api/v1/tours", (req, res) => {
+  const newId = tours[tours.length - 1].id + 1;
+  const newTour = Object.assign({ id: newId }, req.body);
+
+  tours.push(newTour);
+
+  fs.writeFile(
+    `${__dirname}/data/tours-simple.json`,
+    JSON.stringify(tours),
+    (err) => {
+      if (err) console.error(err);
+      res.status(201).json({
+        status: "success",
+        data: { tours: newTour },
+      });
+    }
+  );
 });
 
 const port = process.env.PORT || 8000;
